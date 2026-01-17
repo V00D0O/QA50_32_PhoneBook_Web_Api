fb3e1c27554a4e7a70692438379aadf3fe56104c
pass12345 <tipografia116@gmail.com>
2026-01-17 19:42:39 +0200

    LoginPage

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,4 +1,18 @@
 package ui_tests;
 
-public class LoginTests {
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends AppManager {
+   @Test
+   public void loginPositiveTest(){
+       System.out.println("first test");
+       HomePage homePage = new HomePage(getDriver());
+       LoginPage loginPage = new LoginPage(getDriver());
+       loginPage.typeLoginRegistrationForm("family@mail.ru", "Family123!" );
+       ;loginPage.clickBtnLoginForm();
+   }
+
 }
