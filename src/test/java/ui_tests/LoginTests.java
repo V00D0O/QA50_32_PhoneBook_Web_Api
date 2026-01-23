d11d78b12a2cb9f3d474a8af4fdf1bf53ac598cd
pass12345 <tipografia116@gmail.com>
2026-01-23 13:20:07 +0200

    Registration Positive Test

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -9,40 +9,40 @@ import pages.HomePage;
 import pages.LoginPage;
 
 public class LoginTests extends AppManager {
-   @Test
-   public void loginPositiveTest(){
-       //System.out.println("first test");
-       HomePage homePage = new HomePage(getDriver());
-       homePage.clickBtnLogin();
-       LoginPage loginPage = new LoginPage(getDriver());
-       loginPage.typeLoginRegistrationForm("family@mail.ru", "Family123!" );
-       loginPage.clickBtnLoginForm();
-       Assert.assertTrue(new ContactPage(getDriver()).isTextInBtnAddPresent("ADD"),"validate text");
+    @Test
+    public void loginPositiveTest(){
+        // System.out.println("first test");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm("family@mail.ru",
+                "Family123!");
+        loginPage.clickBtnLoginForm();
+        Assert.assertTrue(new ContactPage(getDriver())
+                .isTextInBtnAddPresent("ADD"));
+    }
 
-   }
     @Test
     public void loginPositiveTestWithUser(){
-        User user = new User("family@mail.ru", "Family123!");
+        User user = new User("family@mail.ru",
+                "Family123!");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnLoginForm();
         Assert.assertTrue(new ContactPage(getDriver()).isTextInBtnSignOutPresent("Sign Out"));
-
     }
 
     @Test
     public void loginNegativeTest_WrongEmail(){
-       User user = new User("familymail.ru", "Family123!");
+        User user = new User("familymail.ru", "Family123!");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnLoginForm();
-        Assert.assertEquals(loginPage.closeAlertReturnText(), "Wrong email or password");
-
-
+        Assert.assertEquals(loginPage.closeAlertReturnText(),
+                "Wrong email or password");
     }
-
 }
