35772c37a19718f22a19bfdbd49001d766a68b76
pass12345 <tipografia116@gmail.com>
2026-01-30 12:38:31 +0200

    Registration Negative Test HeaderMenuItem

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,16 +6,18 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
-public class HomePage extends BasePage{
- public HomePage(WebDriver driver){
-     setDriver(driver);
-     driver.get("https://telranedu.web.app/home");
-     PageFactory.initElements(new AjaxElementLocatorFactory(driver,10 ), this);
- }
-@FindBy(xpath = "//a[text()='LOGIN']")
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        setDriver(driver);
+        driver.get("https://telranedu.web.app/home");
+        PageFactory.initElements(new AjaxElementLocatorFactory
+                (driver, 10), this);
+    }
+
+    @FindBy(xpath = "//a[text()='LOGIN']")
     WebElement btnLogin;
 
- public void clickBtnLogin(){
-     btnLogin.click();
- }
+    public void clickBtnLogin(){
+        btnLogin.click();
+    }
 }
--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -7,44 +7,50 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
     public LoginPage(WebDriver driver) {
         setDriver(driver);
-        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
+        PageFactory.initElements(new AjaxElementLocatorFactory
+                (driver, 10), this);
     }
-    @FindBy(xpath = " //input[@name='email']")
+
+    @FindBy(xpath = "//input[@name='email']")
     WebElement inputEmail;
     @FindBy(xpath = "//input[@placeholder='Password']")
     WebElement inputPassword;
     @FindBy(xpath = "//button[text()='Login']")
     WebElement btnLoginForm;
-    @FindBy(css= "button[name='registration']")
+    @FindBy(css = "button[name='registration']")
     WebElement btnRegistrationForm;
 
-    public void typeLoginRegistrationForm(String email, String password){
+    public void typeLoginRegistrationForm(String email,
+                                          String password) {
         inputEmail.sendKeys(email);
         inputPassword.sendKeys(password);
-
     }
+
     public void typeLoginRegistrationFormWithUser(User user){
         inputEmail.sendKeys(user.getUsername());
         inputPassword.sendKeys(user.getPassword());
     }
 
-    public  void clickBtnLoginForm(){
+    public void clickBtnLoginForm() {
         btnLoginForm.click();
     }
-    public void clickBtnRegistrationForm(){
+
+    public void clickBtnRegistrationForm() {
         btnRegistrationForm.click();
+        pause(3);
     }
+
     public String closeAlertReturnText(){
-        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.alertIsPresent());
+        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.alertIsPresent());
         String text = alert.getText();
         alert.accept();
         return text;
